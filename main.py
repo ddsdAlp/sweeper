7e2c05a73e45e4c8d0fe56e275d81158d7a73ed7
Alp Çetintaş <alp35cetintas@gmail.com>
2026-01-14 00:38:22 +0300

    Complex Patterns, Logging, Auto Stop

--- main.py
+++ main.py
@@ -5,21 +5,28 @@ from dataclasses import dataclass, field
 
 import mouse
 
+log = open("logfile.txt", "w")
+
 @dataclass
 class Tile:
     state: str
-    bombGroupCount: int
-    bombGroup: list = field(default_factory=list)
+    closedCount: int
+    flaggedCount: int
+    closedGroup: list = field(default_factory=list)
 
 # MEDIUM BOARD - 16x16
 BOARD_TOP = 493
 BOARD_LEFT = 878
 BOARD_WIDTH = 576
 BOARD_HEIGHT = 576
+EMOJI_TOP = 405
+EMOJI_LEFT = 1139
+EMOJI_WIDTH = 55
+EMOJI_HEIGHT = 55
 
 ROWS, COLS = 16, 16
 CELL_SIZE = BOARD_WIDTH // ROWS # 36
-BOARD = [[Tile(state="-", bombGroupCount=0, bombGroup=[]) for i in range(COLS)] for j in range(ROWS)]
+BOARD = [[Tile(state="-", closedCount=0, flaggedCount=0, closedGroup=[]) for i in range(COLS)] for j in range(ROWS)]
 
 colorDict = {
     (112, 120, 128) : "closed",
@@ -93,17 +100,44 @@ def getNeighbours(row, col):
     
     return neighbours
 
-def checkTileCounts(neighbours):
+def getClosedNeighbours(row, col):
+    closedNeighbours = []
+    neighbours = getNeighbours(row, col)
+
+    for i, j in neighbours:
+        if BOARD[i][j].state == "-":
+            closedNeighbours.append((i,j))
+    
+    return closedNeighbours
+
+def checkBombPairs(neighbours):
+    temp = []
+    for i, j in neighbours:
+
+        if BOARD[i][j].state in ["-", "0", "*"]:
+            continue
+
+        if int(BOARD[i][j].state) - BOARD[i][j].flaggedCount == 1:
+            temp.append((i,j))
+    
+    return temp
+
+# get the closedCount, flaggedCount, closedGroup attributes
+def getTileCounts(row, col, neighbours):
     closedCount = 0
     flaggedCount = 0
+    closedGroup = []
 
     for i, j in neighbours:
         if BOARD[i][j].state == "-":
             closedCount += 1
+            closedGroup.append((i,j))
         elif BOARD[i][j].state == "*":
             flaggedCount += 1
     
-    return closedCount, flaggedCount
+    BOARD[row][col].closedCount = closedCount
+    BOARD[row][col].flaggedCount = flaggedCount
+    BOARD[row][col].closedGroup = closedGroup
 
 # =========== Action Functions ===========
 
@@ -113,12 +147,22 @@ def openClosedNeighbours(neighbours):
             BOARD[i][j].state = "-1"
             mouse.leftClickCell(i, j)
 
+def openTile(row, col):
+    if BOARD[row][col].state == "-":
+        BOARD[row][col].state = "-1"
+        mouse.leftClickCell(row, col)
+
 def flagClosedNeighbours(neighbours):
     for i, j in neighbours:
         if BOARD[i][j].state == "-":
             BOARD[i][j].state = "*"
             mouse.rightClickCell(i, j)
 
+def flagTile(row, col):
+    if BOARD[row][col].state == "-":
+        BOARD[row][col].state = "*"
+        mouse.rightClickCell(row, col)
+    
 # ========================================
 
 def processBoard():
@@ -133,14 +177,55 @@ def processBoard():
             # get all neighbour coords
             neighbours = getNeighbours(row, col)
 
-            # number of closed and flagged neighbours
-            closed, flagged = checkTileCounts(neighbours)
+            # number of closed, flagged neighbours updated
+            getTileCounts(row, col, neighbours)
 
-            if int(tile.state) == closed + flagged:
+            if int(tile.state) == tile.closedCount + tile.flaggedCount:
                 flagClosedNeighbours(neighbours)
             
-            if int(tile.state) == flagged:
-                openClosedNeighbours(neighbours)                 
+            if int(tile.state) == tile.flaggedCount:
+                openClosedNeighbours(neighbours)
+            
+            # check if any neighbour has reduced bomb count of 1
+            # if so, check their closedGroup
+            # if int(closedNeigbours - closedGroup) = int(state), flag all
+
+            closedNeighbours = tile.closedGroup
+            possibleNeighbours = checkBombPairs(neighbours)
+
+            for i, j in possibleNeighbours:
+                possibleGroup = getClosedNeighbours(i, j)
+                
+                if len(possibleGroup) <= 1:
+                    continue
+
+                # check if all possibleGroup is in closedNeighbours
+                if set(possibleGroup).issubset(set(closedNeighbours)):
+                    result = list(set(closedNeighbours) - set(possibleGroup))
+
+                    if len(result) == 0:
+                        continue
+                    
+                    text = "Coord: (" + str(row) + "," + str(col) + ")\nResults: " + str(result)
+                    log.write(text)
+                    text = "\nClosed: " + str(tile.closedCount) + " Flagged: " + str(tile.flaggedCount)
+                    log.write(text)
+                    text = "\nMain Cell Closed Neighbours: " + str(closedNeighbours)
+                    log.write(text)
+                    text = "\nNeighbours Containing Max One Mine: " + str(possibleGroup) + "\n\n"
+                    log.write(text)
+
+                    if int(tile.state) == 1 and tile.flaggedCount == 0:
+                        for resRow, resCol in result:
+                            openTile(resRow, resCol)
+                    
+                    if int(tile.state) == 2 and tile.flaggedCount == 1:
+                        for resRow, resCol in result:
+                            openTile(resRow, resCol)
+                    
+                    if int(tile.state) - tile.flaggedCount == len(result) + 1:
+                        for resRow, resCol in result:
+                            flagTile(resRow, resCol)
 
 # take screenshot of the board
 with mss.mss() as sct:
@@ -148,18 +233,22 @@ with mss.mss() as sct:
     monitor = {"top": BOARD_TOP, "left": BOARD_LEFT, "width": BOARD_WIDTH, "height": BOARD_HEIGHT}
     output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
 
-    # Grab the data
-    sct_img = sct.grab(monitor)
-    ssToArr(sct_img)
+    monitor2 = {"top": EMOJI_TOP, "left": EMOJI_LEFT, "width": EMOJI_WIDTH, "height": EMOJI_HEIGHT}
+    output2 = "sct-{top}x{left}_{width}x{height}.png".format(**monitor2)
     
-    loop = 0
-    while loop < 10:
-        loop += 1
+    while True:
+        # Grab the data
+        ss_board = sct.grab(monitor)
+        ss_emoji = sct.grab(monitor2)
+        ssToArr(ss_board)
+
+        if ss_emoji.pixel(33, 20) == (0,0,0):
+            break
 
         processBoard()
 
-        sct_img = sct.grab(monitor)
-        ssToArr(sct_img)
-
     # Save to the picture file
-    mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+    mss.tools.to_png(ss_board.rgb, ss_board.size, output=output)
+    mss.tools.to_png(ss_emoji.rgb, ss_emoji.size, output=output2)
+
+log.close()
